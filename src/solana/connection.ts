1ccfb02c1ae97e1f91ef09cd023f3cfb3abf939e
Sparrow <benjaminthorpe19@gmail.com>
2025-08-19 12:17:05 +0000

    react -> svelte

--- src/solana/connection.ts
+++
@@ -1,8 +0,0 @@
-import { Connection, PublicKey, clusterApiUrl } from "@solana/web3.js";
-
-export const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
-
-// Replace this with your actual deployed program ID
-export const PROGRAM_ID = new PublicKey(
-  "JAVuBXeBZqXNtS73azhBDAoYaaAFfo4gWXoZe2e7Jf8H",
-);
