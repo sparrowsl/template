246c16047477a08007a0aab1129bcb770d55ea90
Sparrow <benjaminthorpe19@gmail.com>
2025-08-19 13:25:37 +0000

    chore: basic homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,38 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	const links = [
+		{ label: 'Solana Docs', href: 'https://docs.solana.com/' },
+		{ label: 'Solana Faucet', href: 'https://faucet.solana.com/' },
+		{ label: 'Solana Cookbook', href: 'https://solana.com/developers/cookbook/' },
+		{ label: 'Solana Stack Overflow', href: 'https://solana.stackexchange.com/' },
+		{ label: 'Solana Developers GitHub', href: 'https://github.com/solana-developers/' }
+	];
+</script>
+
+<div>
+	<div class="flex flex-row justify-center py-[16px] md:py-[64px]">
+		<div class="text-center">
+			<div class="max-w-2xl">
+				<h1 class="text-5xl font-bold">gm</h1>
+				<p class="pt-4 md:py-6">Say hi to your new Solana app.</p>
+			</div>
+		</div>
+	</div>
+
+	<div class="mx-auto max-w-xl py-6 text-center sm:px-6 lg:px-8">
+		<div class="space-y-2">
+			<p>Here are some helpful links to get you started.</p>
+			{#each links as link}
+				<div>
+					<a
+						href={link.href}
+						class="hover:text-gray-500 dark:hover:text-gray-300"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						{link.label}
+					</a>
+				</div>
+			{/each}
+		</div>
+	</div>
+</div>
